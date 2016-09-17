89d7d64b8861da08c276f54a84f6d45aa967f3ed
infected-mushroom <ksenia.valchuk@gmail.com>
2016-09-17 23:58:04 +0300

    Form for a new board is added

--- boards/urls.py
+++ boards/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^(?P<board_id>[0-9]+)/lists/$', views.board_content, name='board_content'),
     # ex: /boards/5/lists/1
     url(r'^(?P<board_id>[0-9]+)/lists/(?P<list_id>[0-9]+)/$', views.list_content, name='list_content'),
+    url(r'^new/$', views.board_new, name='board_new'),
     # ex: /polls/5/vote/
     #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404
 from django.http import HttpResponse
+from datetime import datetime
 #from django.template import loader
 
 from .models import Board, List
+from .forms import BoardForm
 
 def index(request):
-    boards = Board.objects.order_by('-creation_date')[:5]
+    boards = Board.objects.order_by('-creation_date')[:10]
     context = {
         'boards': boards,
     }
@@ -27,3 +29,17 @@ def list_content(request, board_id, list_id):
     return render(request, 'boards/list_content.html', {'list': list})
 #    response = "You're looking at list %s."
 #    return HttpResponse(response % list_id)
+
+
+def board_new(request):
+    if request.method == "POST":
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            board = form.save(commit=False)
+            board.creation_date = datetime.now()
+            board.save()
+            return redirect('/boards/%s/lists/' %board.pk, pk=board.pk)
+#            return redirect('boards/board_content', pk=board.pk)
+    else:
+        form = BoardForm()
+    return render(request, 'boards/board_new.html', {'form': form})
